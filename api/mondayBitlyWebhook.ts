003799159345cfefbb7e147ec80f9d7a5a1c6685
Colton Colcleasure <colshacol@gmail.com>
2020-03-23 21:10:41 -0500

    Fixed endpoint to handle multiple methods.

--- api/mondayBitlyWebhook.ts
+++ api/mondayBitlyWebhook.ts
@@ -1,6 +1,29 @@
 import { NowRequest, NowResponse } from "@now/node"
 
+const handlePost = (req, res) => {
+  const { challenge } = req.body
+  return challenge ? handleChallenge(req, res) : handleEvent(req, res)
+}
+
+const handleEvent = (req, res) => {
+  res.json({})
+}
+
 export default (req: NowRequest, res: NowResponse) => {
-  console.log("body:", req.body)
-  res.json({ challenge: req.body.challenge })
+  switch (req.method) {
+    case "POST":
+      return handlePost(req, res)
+    case "GET":
+      return handleGet(req, res)
+    default:
+      return res.json({ error: "unsupported request method" })
+  }
+}
+
+const handleChallenge = (req, res) => {
+  res.json({ challenge: res.body.challenge })
+}
+
+const handleGet = (req, res) => {
+  res.json({ didItWork: true })
 }
