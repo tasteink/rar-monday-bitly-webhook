6396a2c707e503fb5af53e7d4a8a56744cd433f2
Colton Colcleasure <colshacol@gmail.com>
2020-03-23 20:47:41 -0500

    Fixed webhook to verify with Monday.

--- api/mondayBitlyWebhook.ts
+++ api/mondayBitlyWebhook.ts
@@ -2,5 +2,5 @@ import { NowRequest, NowResponse } from "@now/node"
 
 export default (req: NowRequest, res: NowResponse) => {
   console.log("body:", req.body)
-  res.json({ name: "John", email: "john@example.com" })
+  res.json({ challenge: req.body.challenge })
 }
